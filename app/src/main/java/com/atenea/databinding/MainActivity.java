a310883bf7d091a33086cddad8e3c8497df13804
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-11-22 21:34:00 -0500

    android aplication finish

--- app/src/main/java/com/atenea/databinding/MainActivity.java
+++ app/src/main/java/com/atenea/databinding/MainActivity.java
@@ -1,14 +1,26 @@
 package com.atenea.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
+import com.atenea.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private MyViewModel viewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+        binding.setViewModel(viewModel);
+
+        binding.setLifecycleOwner(this);
+
     }
 }
---
+++ app/src/main/java/com/atenea/databinding/MyViewModel.java
@@ -0,0 +1,14 @@
+package com.atenea.databinding;
+
+import androidx.databinding.ObservableField;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    public ObservableField<String> message = new ObservableField<>("Hello");
+
+    public void updateMessage()
+    {
+        message.set("Hello, Data biding");
+    }
+
+}
